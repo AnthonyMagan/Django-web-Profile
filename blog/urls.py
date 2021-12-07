f7e633ad2d8acb17a9f493d7ed5ad41b331d259a
AnthonyMagan <Anthony.Magan@gmail.com>
2021-12-07 11:40:14 +0000

    Second commit!

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     #path('hello', views.all_blogs, name='all_blogs'),  #you can now reach this blog by using localhost:8000/blog/hello. This is useful if you have alot of app from a web-application
-    #add a new path S5L46. Dynamically updates the int passing the numver to details
+    #add a new path S5L46. Dynamically updates the int passing the number to details
     path('<int:blog_id>/', views.detail, name='detail'),
 ]
